97929ddc55a1bdd9bf16b7fb1a80e6f4603f5d5c
Igor432 <igomar0602@gmail.com>
2022-08-17 16:08:43 +0300

    home page added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect, useContext } from 'react';
+
 const Home = () => {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    getTrending();
+  }, []);
+
+  async function getTrending() {
+    const trends = await axios.get(
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=335e18ee033f463b61f137f6ef07bd65'
+    );
+    setTrending(trends.data.results);
+    console.log(trending);
+  }
+
   return (
     <main>
       <h1>Trending Todays</h1>
       <img src="https://via.placeholder.com/960x240" alt="" />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-        voluptatum voluptate.
-      </p>
+
+      <ul>
+        {trending.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
     </main>
   );
 };
